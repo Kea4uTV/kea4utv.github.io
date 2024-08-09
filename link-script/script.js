10eaac40f52483333658a1845ef12b240590c94f
Kea4uTV <143631618+Kea4uTV@users.noreply.github.com>
2024-08-09 14:27:15 +0700

    Update script.js

--- link-script/script.js
+++ link-script/script.js
@@ -4,13 +4,6 @@ const tracks = [
     // Add more tracks here
 ];
 
-/*const popupContents = [
-    { type: 'video', src: 'https://khmermov.com/videos/188_4-6-24.mp4' },
-    { type: 'image', src: 'https://static-01.daraz.pk/p/b9f9d5ac8eda5ae8e7c0813b62806074.jpg' },
-    { type: 'gif', src: 'https://www.activecampaign.com/wp-content/uploads/2021/10/lt5bzx5k_popups_3.gif' },
-    { type: 'text', text: '«យើងទាំងអស់គ្នា រួមគ្នាបំបាត់គ្រឿងញៀន»' }
-];/*
-
 const popupContents = [
     {
         type: 'video',
