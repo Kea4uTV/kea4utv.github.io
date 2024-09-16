c02b92c891d8da7a1d016c9dac01a36345b0c91b
Kea4uTV <143631618+Kea4uTV@users.noreply.github.com>
2024-09-16 18:34:42 +0700

    Add files via upload

---
+++ askdlfka's;lk'ld;kfpoeka';lsdk'f;lka'sldkoieqiuiwoeuproiqupwoiueiruqpiw893rq/Convert-Text-into-Video/jquery_p.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ 
+  $.get("https://convert2video.com/Convert-Text-into-Video/divbackground.htm", function(data) {
+    $("#divbackground").html(data);
+  });
+  
+  
+}); 
+
