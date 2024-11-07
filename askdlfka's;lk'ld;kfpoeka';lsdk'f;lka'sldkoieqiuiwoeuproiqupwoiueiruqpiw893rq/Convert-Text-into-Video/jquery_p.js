e5f9bf4e421268acfb4fdc2815ea373a5029e301
Kea4uTV <143631618+Kea4uTV@users.noreply.github.com>
2024-11-07 17:32:17 +0700

    Delete askdlfka's;lk'ld;kfpoeka';lsdk'f;lka'sldkoieqiuiwoeuproiqupwoiueiruqpiw893rq directory

--- askdlfka's;lk'ld;kfpoeka';lsdk'f;lka'sldkoieqiuiwoeuproiqupwoiueiruqpiw893rq/Convert-Text-into-Video/jquery_p.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
- 
-  $.get("https://convert2video.com/Convert-Text-into-Video/divbackground.htm", function(data) {
-    $("#divbackground").html(data);
-  });
-  
-  
-}); 
-
